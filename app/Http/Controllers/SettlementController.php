bf8e2b67cdb5b844e9baa4d57593a5c72af178de
Munirul Haikal Bin Hasbulmunir <munirulhaikal@gmail.com>
2024-02-02 10:38:14 +0800

    Add base module

---
+++ app/Http/Controllers/SettlementController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Settlement;
+use Illuminate\Http\Request;
+
+class SettlementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Settlement $settlement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Settlement $settlement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Settlement $settlement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Settlement $settlement)
+    {
+        //
+    }
+}
