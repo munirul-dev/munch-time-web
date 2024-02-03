112ef0ba7a31e82579688d50e96fa2a375ccc705
Munirul Haikal Bin Hasbulmunir <munirulhaikal@gmail.com>
2024-02-03 13:00:05 +0800

    Update authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        try {
+            $credentials = $request->getCredentials();
+            if (!Auth::validate($credentials)) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Username & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->username)->first();
+            return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfully',
+                'token' => $user->createToken(Hash::make($user->email))->plainTextToken,
+                'data' => new UserResource($user),
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
